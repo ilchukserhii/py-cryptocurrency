de28229ee770043bb390fe4acd0d41806a502fe0
Seroga <ilchuk.cergey@gmail.com>
2026-02-04 15:21:35 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,16 @@
-# write your code here
+import pytest
+import app.main as main
+
+
+@pytest.mark.parametrize(
+    "exchange_rate, expected",
+    [
+        (0.95, "Do nothing"),
+        (1.05, "Do nothing"),
+        (0.94, "Sell all your cryptocurrency"),
+        (1.06, "Buy more cryptocurrency")
+    ]
+)
+def test_cryptocurrency_action(monkeypatch, exchange_rate: int | float, expected: str) -> None:
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda current_rate: current_rate * exchange_rate)
+    assert main.cryptocurrency_action(100) == expected
